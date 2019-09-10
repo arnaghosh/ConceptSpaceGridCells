6adb09b4301a9f00621b03b93ce112a52d6929b6
arnaghosh <arnatubaikgp@gmail.com>
2019-09-10 17:36:27 -0400

    adding pre-final layer activations in network space

--- plot_activations.py
+++ plot_activations.py
@@ -3,46 +3,26 @@ import seaborn as sns
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 
+np.random.seed(13)
 zipfile = np.load('./results/MNIST_data_target_activations.npz')
 data_arr = zipfile['data_arr']
 target_arr = zipfile['target_arr']
 h1_arr = zipfile['h1_arr']
 h2_arr = zipfile['h2_arr']
-zipfile2 = np.load('./results/TSNE_pos.npz')
+# zipfile2 = np.load('./results/TSNE_pos.npz')
+zipfile2 = np.load('./results/MNIST_network_space_tsne_pos.npz')
 tsne_loc = zipfile2['tsne_pos']
 
+h1_arr_pos = h1_arr
 h2_arr_pos = h2_arr*(h2_arr>=0)
+start_neuron=25
+vmin=h1_arr_pos.min()
+vmax=h1_arr_pos.max()
 plt.figure()
-plt.subplot(2,5,1)
-# sns.scatterplot(x=tsne_loc[:,0],y=tsne_loc[:,1],hue=h2_arr[:,0],alpha=0.6)
-# plt.title('Original activations of Final layer Neuron 0')
-sns.scatterplot(x=tsne_loc[:,0],y=tsne_loc[:,1],hue=h2_arr_pos[:,0],alpha=0.6)
-plt.title('Filtered activations of Final layer Neuron 0')
-plt.subplot(2,5,2)
-sns.scatterplot(x=tsne_loc[:,0],y=tsne_loc[:,1],hue=h2_arr_pos[:,1],alpha=0.6)
-plt.title('Filtered activations of Final layer Neuron 1')
-plt.subplot(2,5,3)
-sns.scatterplot(x=tsne_loc[:,0],y=tsne_loc[:,1],hue=h2_arr_pos[:,2],alpha=0.6)
-plt.title('Filtered activations of Final layer Neuron 2')
-plt.subplot(2,5,4)
-sns.scatterplot(x=tsne_loc[:,0],y=tsne_loc[:,1],hue=h2_arr_pos[:,3],alpha=0.6)
-plt.title('Filtered activations of Final layer Neuron 3')
-plt.subplot(2,5,5)
-sns.scatterplot(x=tsne_loc[:,0],y=tsne_loc[:,1],hue=h2_arr_pos[:,4],alpha=0.6)
-plt.title('Filtered activations of Final layer Neuron 4')
-plt.subplot(2,5,6)
-sns.scatterplot(x=tsne_loc[:,0],y=tsne_loc[:,1],hue=h2_arr_pos[:,5],alpha=0.6)
-plt.title('Filtered activations of Final layer Neuron 5')
-plt.subplot(2,5,7)
-sns.scatterplot(x=tsne_loc[:,0],y=tsne_loc[:,1],hue=h2_arr_pos[:,6],alpha=0.6)
-plt.title('Filtered activations of Final layer Neuron 6')
-plt.subplot(2,5,8)
-sns.scatterplot(x=tsne_loc[:,0],y=tsne_loc[:,1],hue=h2_arr_pos[:,7],alpha=0.6)
-plt.title('Filtered activations of Final layer Neuron 7')
-plt.subplot(2,5,9)
-sns.scatterplot(x=tsne_loc[:,0],y=tsne_loc[:,1],hue=h2_arr_pos[:,8],alpha=0.6)
-plt.title('Filtered activations of Final layer Neuron 8')
-plt.subplot(2,5,10)
-sns.scatterplot(x=tsne_loc[:,0],y=tsne_loc[:,1],hue=h2_arr_pos[:,9],alpha=0.6)
-plt.title('Filtered activations of Final layer Neuron 9')
+for i in range(25):
+	plt.subplot(5,5,i+1)
+	sns.scatterplot(x=tsne_loc[:,0],y=tsne_loc[:,1],hue=h1_arr_pos[:,start_neuron+i], alpha=0.6)
+	# plt.scatter(x=tsne_loc[:,0],y=tsne_loc[:,1],c=h1_arr_pos[:,start_neuron+i], alpha=0.6,vmin=vmin,vmax=vmax)
+	plt.title('Filtered activations of Pre-Final layer Neuron '+str(start_neuron+i))
+# plt.legend()
 plt.show()
